bb9965596db9dea6b67deb3970f13702a2816e64
Wladek <Wladek>
2018-11-21 15:49:36 -0200

    acertos do tripulantes search

--- src/app/tripulante/tripulante.component.ts
+++ src/app/tripulante/tripulante.component.ts
@@ -14,12 +14,30 @@ export class TripulanteComponent implements OnInit {
   }
 
   tripulantes: Array<any>;
+  tripulantesFilter: Array<any>;
+  nome = '';
+  trato = '';
+  anac = '';
 
   ngOnInit() {
     this.api.getNTripulante().then(response => {
-      this.tripulantes = response;
-      console.log(this.tripulantes);
+      this.tripulantesFilter = this.tripulantes = response;
     });
+  }
+
+  search() {
+    this.tripulantes = this.tripulantesFilter.filter((trip) => {
+      if (!this.nome && !this.trato && !this.anac) {
+        return true;
+      }
+      return this.trato && trip.Trato.toLowerCase().includes(this.trato.toLowerCase()) ||
+        this.nome && trip.NomeCompleto.toLowerCase().includes(this.nome.toLowerCase()) ||
+        this.anac && trip.CodigoANAC.toString().includes(this.anac);
+    });
+  }
+
+  saveTripulante() {
+    console.log(this.tripulantesFilter);
   }
 
 }
